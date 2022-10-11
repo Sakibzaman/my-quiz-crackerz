980446e5a99614e38090f09db04565c48284807c
Sakib Zaman <sakibzaman19@gmail.com>
2022-10-11 16:58:30 +0600

    configured tailwind css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className=" bg-slate-500">this is app.js</h1>
+    </div>
+  );
 }
 
 export default App;
